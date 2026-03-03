5e72712698cbf1c594cd5a9056fdac4544cae0aa
oldprinted-arch <oldprinted@gmail.com>
2026-03-03 13:10:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,52 @@
-import { findByProps } from "@vendetta/metro";
-import { after } from "@vendetta/patcher";
-import { React } from "@vendetta/metro/common";
-import { getChannelId } from "@vendetta/ui";
-
-const MessageActions = findByProps("sendMessage");
-const ChatInput = findByProps("ChatInput");
-
-let unpatch;
+// Bunny / Vendetta compatible simple plugin
 
 export default {
   onLoad() {
-    unpatch = after("default", ChatInput, (_, res) => {
-      try {
-        const children = res?.props?.children;
-        if (!Array.isArray(children)) return res;
+    try {
+      const metro = globalThis.vendetta.metro;
+      const patcher = globalThis.vendetta.patcher;
+      const React = globalThis.vendetta.metro.common.React;
+      const ui = globalThis.vendetta.ui;
 
-        const button = React.createElement(
-          "TouchableOpacity",
-          {
-            onPress: () => {
-              const channelId = getChannelId();
-              MessageActions.sendMessage(channelId, {
-                content: "привет как дела"
-              });
+      const MessageActions = metro.findByProps("sendMessage");
+      const ChatInput = metro.findByProps("ChatInput");
+
+      this.unpatch = patcher.after("default", ChatInput, (_, res) => {
+        try {
+          const children = res?.props?.children;
+          if (!Array.isArray(children)) return res;
+
+          const button = React.createElement(
+            "TouchableOpacity",
+            {
+              onPress: () => {
+                const channelId = ui.getChannelId();
+                MessageActions.sendMessage(channelId, {
+                  content: "привет как дела"
+                });
+              },
+              style: {
+                marginRight: 8,
+                padding: 8
+              }
             },
-            style: {
-              marginRight: 8,
-              padding: 8
-            }
-          },
-          React.createElement("Text", null, "Hi")
-        );
+            React.createElement("Text", null, "Hi")
+          );
 
-        children.unshift(button);
-      } catch (e) {
-        console.log(e);
-      }
+          children.unshift(button);
+        } catch (e) {
+          console.log(e);
+        }
 
-      return res;
-    });
+        return res;
+      });
+
+    } catch (e) {
+      console.log("Plugin load error:", e);
+    }
   },
 
   onUnload() {
-    unpatch?.();
+    this.unpatch?.();
   }
 };
