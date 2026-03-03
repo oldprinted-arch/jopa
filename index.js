2da87d3d9b912239aca840a9074bb1be793b4298
oldprinted-arch <oldprinted@gmail.com>
2026-03-03 13:12:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,49 @@
-// Bunny / Vendetta compatible simple plugin
+// ==UserPlugin==
+// @name HelloButton
+// @description Test send button
+// @author You
+// ==/UserPlugin==
 
-export default {
-  onLoad() {
-    try {
-      const metro = globalThis.vendetta.metro;
-      const patcher = globalThis.vendetta.patcher;
-      const React = globalThis.vendetta.metro.common.React;
-      const ui = globalThis.vendetta.ui;
+(function() {
+  const metro = globalThis.vendetta?.metro || globalThis.bunny?.metro;
+  const patcher = globalThis.vendetta?.patcher || globalThis.bunny?.patcher;
+  const React = metro.common.React;
+  const ui = globalThis.vendetta?.ui || globalThis.bunny?.ui;
+  const MessageActions = metro.findByProps("sendMessage");
+  const ChatInput = metro.findByProps("ChatInput");
 
-      const MessageActions = metro.findByProps("sendMessage");
-      const ChatInput = metro.findByProps("ChatInput");
+  let unpatch;
 
-      this.unpatch = patcher.after("default", ChatInput, (_, res) => {
-        try {
-          const children = res?.props?.children;
-          if (!Array.isArray(children)) return res;
+  function load() {
+    unpatch = patcher.after("default", ChatInput, (_, res) => {
+      try {
+        const children = res?.props?.children;
+        if (!Array.isArray(children)) return res;
 
-          const button = React.createElement(
-            "TouchableOpacity",
-            {
-              onPress: () => {
-                const channelId = ui.getChannelId();
-                MessageActions.sendMessage(channelId, {
-                  content: "привет как дела"
-                });
-              },
-              style: {
-                marginRight: 8,
-                padding: 8
-              }
+        const button = React.createElement(
+          "TouchableOpacity",
+          {
+            onPress: () => {
+              const channelId = ui.getChannelId();
+              MessageActions.sendMessage(channelId, {
+                content: "привет как дела"
+              });
             },
-            React.createElement("Text", null, "Hi")
-          );
+            style: { marginRight: 8, padding: 8 }
+          },
+          React.createElement("Text", null, "Hi")
+        );
 
-          children.unshift(button);
-        } catch (e) {
-          console.log(e);
-        }
-
-        return res;
-      });
-
-    } catch (e) {
-      console.log("Plugin load error:", e);
-    }
-  },
-
-  onUnload() {
-    this.unpatch?.();
+        children.unshift(button);
+      } catch (e) { console.log(e); }
+      return res;
+    });
   }
-};
+
+  function unload() {
+    unpatch?.();
+  }
+
+  load();
+  globalThis.helloButtonUnload = unload;
+})();
